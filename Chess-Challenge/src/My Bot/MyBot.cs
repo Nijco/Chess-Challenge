a9e92c44f51ed15ab213025aff54dd18db14d705
Nijco <heijnk@posteo.de>
2023-07-23 01:14:47 +0200

    my bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,12 +2,18 @@
 using System.Linq;
 using System.Numerics;
 using System;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
+    bool weAreWhite = true;
+    List<Move> mList = new List<Move>();
     public Move Think(Board board, Timer timer)
     {
-        return MinMax(board,5,int.MinValue,int.MaxValue,true).Item1!.Value;
+        weAreWhite = board.IsWhiteToMove;
+        mList = new List<Move>();
+        Move? move = MinMax(board,4,int.MinValue,int.MaxValue,true).Item1;
+        return move.Value;
     }
 
     public (Move?,int) MinMax(Board b,int depth,double alpha, double beta,bool maximizingPlayer){
@@ -15,52 +21,62 @@ public class MyBot : IChessBot
             return (null,this.eval(b));
         }
         if (maximizingPlayer){
-            var maxEval = int.MinValue;
+            int? maxEval = null;
             Move? maxMove = null;
             foreach(var child in b.GetLegalMoves().OrderBy(m=>m.IsCapture ? 0 : 1)){
+                mList.Add(child);
                 b.MakeMove(child);
                 var eval = MinMax(b,depth-1,alpha,beta,false).Item2;
                 b.UndoMove(child);
-                if(eval>maxEval){
+                mList.RemoveAt(mList.Count-1);
+                if(maxEval==null||eval>maxEval){
                     maxMove = child;
                 }
-                maxEval = Math.Max(maxEval,eval);
+                maxEval = Math.Max(maxEval??int.MinValue,eval);
                 alpha = Math.Max(alpha,eval);
                 if(beta <= alpha){
                     break;
                 }
             }
-            return (maxMove,maxEval);
+            if(maxMove==null&&depth==5){
+                Console.WriteLine("what");
+            }
+            return (maxMove,maxEval??int.MinValue);
         }else{
-            var minEval = int.MaxValue;
+            int? minEval = null;
             Move? minMove = null;
             foreach(var child in b.GetLegalMoves().OrderBy(m=>m.IsCapture ? 0 : 1)){
+                mList.Add(child);
                 b.MakeMove(child);
                 var eval = MinMax(b,depth-1,alpha,beta,true).Item2;
                 b.UndoMove(child);
-                if(eval<minEval){
+                mList.RemoveAt(mList.Count-1);
+                if(minEval==null||eval<minEval){
                     minMove = child;
                 }
-                minEval = Math.Min(minEval,eval);
-                beta = Math.Min(alpha,eval);
+                minEval = Math.Min(minEval??int.MaxValue,eval);
+                beta = Math.Min(beta,eval);
                 if(beta <= alpha){
                     break;
                 }
             }
-            return (minMove,minEval);
+            return (minMove,minEval??int.MaxValue);
         }
     }
 
-    public int eval(Board b){
-        return b.GetPieceList(PieceType.Pawn,!b.IsWhiteToMove).Count
-        + b.GetPieceList(PieceType.Bishop,!b.IsWhiteToMove).Count*3
-        + b.GetPieceList(PieceType.Knight,!b.IsWhiteToMove).Count*3
-        + b.GetPieceList(PieceType.Rook,!b.IsWhiteToMove).Count*5
-        + b.GetPieceList(PieceType.Queen,!b.IsWhiteToMove).Count*8
-        -b.GetPieceList(PieceType.Pawn,b.IsWhiteToMove).Count
-        - b.GetPieceList(PieceType.Bishop,b.IsWhiteToMove).Count*3
-        - b.GetPieceList(PieceType.Knight,b.IsWhiteToMove).Count*3
-        - b.GetPieceList(PieceType.Rook,b.IsWhiteToMove).Count*5
-        - b.GetPieceList(PieceType.Queen,b.IsWhiteToMove).Count*8;
+    public int eval(Board b){ //change to isWhiteToMove?
+        if(b.IsInCheckmate()){
+            return 10000 * (b.IsWhiteToMove!=weAreWhite?1:-1);
+        }
+        return b.GetPieceList(PieceType.Pawn,weAreWhite).Count
+        + b.GetPieceList(PieceType.Bishop,weAreWhite).Count*3
+        + b.GetPieceList(PieceType.Knight,weAreWhite).Count*3
+        + b.GetPieceList(PieceType.Rook,weAreWhite).Count*5
+        + b.GetPieceList(PieceType.Queen,weAreWhite).Count*8
+        -b.GetPieceList(PieceType.Pawn,!weAreWhite).Count
+        - b.GetPieceList(PieceType.Bishop,!weAreWhite).Count*3
+        - b.GetPieceList(PieceType.Knight,!weAreWhite).Count*3
+        - b.GetPieceList(PieceType.Rook,!weAreWhite).Count*5
+        - b.GetPieceList(PieceType.Queen,!weAreWhite).Count*8;
     }
 }
